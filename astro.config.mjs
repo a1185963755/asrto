f46c81fef9b048598ba943e6348136f63d932eea
a1185963755 <1185963755@qq.com>
2024-01-30 18:04:00 +0800

    fix build bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
  * @Author: Oliver
  * @Date: 2024-01-29 18:37:14
  * @LastEditors: Oliver
- * @LastEditTime: 2024-01-30 17:29:45
+ * @LastEditTime: 2024-01-30 17:49:28
  * @Description:
  *
  */
 import { defineConfig } from "astro/config";
-
+import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-study.netlify.app",
+  integrations: [preact()],
 });
